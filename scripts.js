655667e9a49b12eef5e70d9f55fa7169938a1729
João Marcos Tomáz Silva Campos <joaomarcoscampos@dcc.ufmg.br>
2024-07-14 11:18:27 -0300

    adicionando textos problema e triangularização

--- scripts.js
+++ scripts.js
@@ -68,13 +68,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     problemDescriptionButton.addEventListener('click', function() {
-        const text = 'a completar';
+        const text = 'A ideia do trabalho é focada no problema da galeria de arte. Imagine a seguinte situação: Você é dono de uma galeria em que vários quadros artísticos e grandes antiguidades são expostas. Sabendo dos perigos existentes, você precisa ter alguma forma de segurança dentro do espaço, como câmeras por exemplo. A ideia do algoritmo é encontrar o menor número de câmeras que vigie toda a sua galeria, sendo esta representada por um polígono.';
         const content = `<h2>Descrição do problema</h2><p>${text}</p>`;
         polygonGraph.innerHTML = content;
     });
 
     triangulationDescriptionButton.addEventListener('click', function() {
-        const text = 'a completar';
+        const text = 'O algoritmo de triangulação utilizado “é chamado ear-clipping”. A ideia é dividir um polígono simples em triângulos. Ele funciona identificando "orelhas" no polígono, onde uma "orelha" é um triângulo formado por três vértices consecutivos que não contém outros vértices do polígono em seu interior. O processo começa verificando se o polígono é simples e, em seguida, identifica e remove iterativamente essas "orelhas", adicionando os triângulos formados à lista de resultados. Cada vez que uma "orelha" é removida, a lista de vértices do polígono é atualizada, como pode ser visto na animação. Esse procedimento continua até que o polígono seja reduzido a três vértices, formando o último triângulo. É importante ressaltar que caso o polígono não seja simples, o algoritmo não funcionará corretamente.';
         const content = `<h2>Descrição da triangulação</h2><p>${text}</p>`;
         polygonGraph.innerHTML = content;
     });
